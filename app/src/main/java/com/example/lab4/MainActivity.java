88df724c18129443373aa2b1b0fb0cbd4c205496
philthethrill <pande021@uottawa.ca>
2021-10-10 19:01:34 -0400

    Sets logo to main after choosing one

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +29,40 @@ public class MainActivity extends AppCompatActivity {
     public void OnSetAvatarButton(View view) {
         Intent inten = new Intent(getApplicationContext(), ProfileActivity.class);
         startActivityForResult(inten,0);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) return;
+        //Getting the Avatar Image we show to our users
+        ImageView avatarImage = (ImageView) findViewById(R.id.logo);
+        //Figuring out the correct image
+        String drawableName = "ic_logo_00";
+        switch (data.getIntExtra("imageID", R.id.logo)) {
+            case R.id.logo000:
+                drawableName = "ic_logo_00";
+                break;
+            case R.id.logo001:
+                drawableName = "ic_logo_01";
+                break;
+            case R.id.logo002:
+                drawableName = "ic_logo_02";
+                break;
+            case R.id.logo003:
+                drawableName = "ic_logo_03";
+                break;
+            case R.id.logo004:
+                drawableName = "ic_logo_04";
+                break;
+            case R.id.logo005:
+                drawableName = "ic_logo_05";
+                break;
+            default:
+                drawableName = "ic_logo_00";
+                break;
+        }
+        int resID = getResources().getIdentifier(drawableName, "drawable",
+                getPackageName());
+        avatarImage.setImageResource(resID);
     }
 }
