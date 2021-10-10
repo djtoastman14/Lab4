6d796ef0237da9e3b6361882b71699b54a8c2c67
djtoastman14 <71289356+djtoastman14@users.noreply.github.com>
2021-10-10 18:19:49 -0400

    fixed app crash

--- app/src/main/java/com/example/lab4/ProfileActivity.java
+++ app/src/main/java/com/example/lab4/ProfileActivity.java
@@ -1,14 +1,14 @@
 package com.example.lab4;
 
-import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_activity);
+        setContentView(R.layout.profile);
     }
-
 }
