f023f85d6c6015cd3403f62dd4d0998b9a5ce777
djtoastman14 <71289356+djtoastman14@users.noreply.github.com>
2021-10-10 16:38:25 -0400

    finished map functionality

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnOpenGoogleMaps(View view) {
+
+        EditText zipCode = (EditText)findViewById(R.id.zipCode);
+        Uri gmmIntentUri = Uri.parse("https://maps.google.co.in/maps?q="+zipCode.getText());
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
