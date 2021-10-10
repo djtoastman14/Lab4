fc5a2469fbc44bfdd84af05db120cc6c973428b6
philthethrill <pande021@uottawa.ca>
2021-10-10 18:27:35 -0400

    setTeamIcon created

--- app/src/main/java/com/example/lab4/ProfileActivity.java
+++ app/src/main/java/com/example/lab4/ProfileActivity.java
@@ -2,7 +2,10 @@ package com.example.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+    }
+    public void setTeamIcon(View view)
+    {
+        Intent returnIntent = new Intent();
+        ImageView selectedImage = (ImageView) view;
+        returnIntent.putExtra("imageID", selectedImage.getId());
+        setResult(RESULT_OK, returnIntent);
+
+        finish();
     }
 }
